558c3d95c596f6581440efa382121c77042dfe85
sysox1 <marek.sys@gmail.com>
2019-02-18 19:25:05 +0100

    hello world added

---
+++ cmake-build-debug/CMakeFiles/Cvicenie1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cvicenie1.dir/main.cpp.obj"
+  "Cvicenie1.pdb"
+  "Cvicenie1.exe"
+  "Cvicenie1.exe.manifest"
+  "libCvicenie1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cvicenie1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Cvicenie1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cvicenie1.
+# This may be replaced when dependencies are built.
